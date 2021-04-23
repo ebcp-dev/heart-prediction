bbc8a6a1b6364bb1d9348c41d3fb3e425f2fcdfc
Earl Perez <ebcp.dev@gmail.com>
2021-04-23 06:41:11 -0700

    updated ml functions

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, jsonify
-from model import predict_test
+from .model import predict
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
+    user_input = []
     if request.method == 'GET':
-        return jsonify({'prediction': predict_test})
-
-
-@app.route('/predict', methods=['GET'])
-def predict():
-    return jsonify({'results': 1})
+        return jsonify({'home': 'show homepage'}), 200
+    # Iterate through user response and add to prediction value array.
+    for key in request.get_json():
+        user_input.append(request.get_json()[key])
+    showpred = predict(user_input).tolist()
+    if request.method == 'POST':
+        return jsonify({'prediction': showpred}), 201
--- app/model.py
+++ app/model.py
@@ -1,18 +1,24 @@
-import numpy as np
 import pandas as pd
 import joblib
 from sklearn.preprocessing import StandardScaler
-import sys
+from sklearn.model_selection import train_test_split
 
 sc = StandardScaler()
 data = pd.read_csv('./model/heart-disease.csv')
 model = joblib.load('./model/randomforestmodel.joblib')
+# Assign 13 features to X, and column 'target' to predictor y
+X = data.iloc[:, :-1].values
+y = data.iloc[:, -1].values
+# Split: the dataset into the Training set and Test set
+x_train, x_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=1)
+# Standardize the data so its distribution will have a mean of 0 and a standard deviation of 1.
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+# Train/Fit model
+model.fit(x_train, y_train)
 
 
-def show_head():
-    return data.head()
-
-
-def predict_test():
-    return model.predict(sc.transform(
-        [[20, 1, 2, 110, 230, 1, 1, 140, 1, 2.2, 2, 0, 2]]))
+def predict(user_input):
+    prediction = model.predict(sc.transform([user_input]))
+    return prediction
